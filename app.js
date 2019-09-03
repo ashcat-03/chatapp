5ab6a6694bc429b36f8361607e2fb092aec31088
Misa Hayakawa <m.ashcat@icloud.com>
2019-09-04 03:14:28 +0900

    added bulma(css framework)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 5001
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render("index")
+  res.render("index",{})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
@@ -18,4 +18,6 @@ install---npm install nodemon -g
 
 (2)ejsとnode
 URL---https://scotch.io/tutorials/use-ejs-to-template-your-node-application
+
+(3)cssのフレームワークbulmaを使用
 */
